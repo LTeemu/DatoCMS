e34b811479e84bd741ddace88d09052174bc1dc2
n9lete00 <n9lete00@students.oamk.fi>
2022-05-04 12:03:28 +0300

    Added nextjs-progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
+			<NextNProgress
+				color='linear-gradient(130deg, rgba(204,21,21,1) 0%, rgba(213,197,22,1) 25%, rgba(20,214,126,1) 50%, rgba(30,62,208,1) 75%, rgba(170,18,210,1) 100%)'
+				height={5}
+			/>
 			<Component {...pageProps} />
 		</Layout>
 	)
