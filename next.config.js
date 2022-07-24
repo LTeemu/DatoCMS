dc4bedfd0bef188ec4d9d5aceb24febf2f5510de
n9lete00 <n9lete00@students.oamk.fi>
2022-07-24 11:32:39 +0300

    Hide and change API

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     domains: ['www.datocms-assets.com', 'cdn.pixabay.com'],
   },
-  env: {
-    KEY: 'f8d319e0ad130ab02b053e07ccbef5',
-  },
 }
 
 module.exports = nextConfig
